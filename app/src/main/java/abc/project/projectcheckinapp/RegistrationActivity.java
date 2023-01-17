d1c8f5f308e9cdc2e46c1e427864ec7d6ef3cc5b
klunCheng <klun.CH88@outlook.com>
2023-01-17 11:47:41 +0800

    20230117 K:註冊頁面初稿、string新增

--- app/src/main/java/abc/project/projectcheckinapp/RegistrationActivity.java
+++ app/src/main/java/abc/project/projectcheckinapp/RegistrationActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import abc.project.projectcheckinapp.databinding.ActivityRegistrationBinding;
+
 public class RegistrationActivity extends AppCompatActivity {
+
+    ActivityRegistrationBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registration);
+        binding = ActivityRegistrationBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
