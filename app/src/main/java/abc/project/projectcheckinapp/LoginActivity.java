6468657817a67a661207c496cc819510dbf0f7a7
Cindy <cindy0909.liu@gmail.com>
2023-01-18 09:00:39 +0800

    test

--- app/src/main/java/abc/project/projectcheckinapp/LoginActivity.java
+++ app/src/main/java/abc/project/projectcheckinapp/LoginActivity.java
@@ -64,7 +64,7 @@ public class LoginActivity extends AppCompatActivity {
 
             }
         }
-    }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
