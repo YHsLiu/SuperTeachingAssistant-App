6ba085e0e3da285a27cd166946499d571344fc1f
cindy <cindy0909.liu@gmail.com>
2023-01-21 12:02:52 +0800

    新增 查詢 方法

--- app/src/main/java/abc/project/projectcheckinapp/rawData/AdapterAllStu.java
+++ app/src/main/java/abc/project/projectcheckinapp/rawData/AdapterAllStu.java
@@ -1,5 +1,7 @@
 package abc.project.projectcheckinapp.rawData;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,17 +14,60 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+
 import abc.project.projectcheckinapp.R;
 
 public class AdapterAllStu extends RecyclerView.Adapter<AdapterAllStu.ViewHolder>{
 
-    JSONArray stuInfos;
+    SQLiteDatabase db;
+    ArrayList<StuModel> result;
+    ClickListener click;
+    int cid;
 
     public AdapterAllStu() {
     }
 
-    public AdapterAllStu(JSONArray stuInfos) {
-        this.stuInfos = stuInfos;
+    public AdapterAllStu(SQLiteDatabase db, ClickListener click, int cid) {
+        this.db = db;
+        this.click = click;
+        this.cid = cid;
+        result = new ArrayList<>();
+        Cursor cursor = db.rawQuery("select * from "+cid+"_allstu;",null);
+        if (cursor.getCount() > 0){
+            cursor.moveToFirst();
+            do {
+                StuModel stuModel = new StuModel(
+                        cursor.getString(0),
+                        cursor.getString(1),
+                        cursor.getString(2));
+                result.add(stuModel);
+            }while ( ( cursor.moveToNext()));
+        }
+        cursor.close();
+    }
+
+    public void setAllColumn(String select){
+        // 輸入查詢 姓名 或 科系 或 學號
+        result.clear();
+        Cursor cursor;
+        if (select.equals("")){
+            cursor = db.rawQuery("select * from "+cid+"_allstu;",null);
+        } else {
+            cursor = db.rawQuery("select * from "+cid+"_allstu where name=? or depart=? or stuId=?;",new String[]{select});
+        }
+        if (cursor.getCount()>0){
+            cursor.moveToFirst();
+            do {
+                StuModel stuModel = new StuModel(
+                        cursor.getString(0),
+                        cursor.getString(1),
+                        cursor.getString(2)
+                );
+                result.add(stuModel);
+            }while (cursor.moveToNext());
+            cursor.close();
+        }
     }
 
     @NonNull
@@ -34,14 +79,10 @@ public class AdapterAllStu extends RecyclerView.Adapter<AdapterAllStu.ViewHolder
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        try {
-            JSONObject info = stuInfos.getJSONObject(position);
-            holder.studentName.setText(info.getString("姓名"));
-            holder.department.setText(info.getString("科系"));
-            holder.studentName.setText(info.getInt("學號"));
-        } catch (JSONException e) {
-            throw new RuntimeException(e);
-        }
+        holder.studentName.setText(result.get(position).getStuname());
+        holder.department.setText(result.get(position).getStudepart());
+        holder.studentName.setText(result.get(position).getStuid());
+
     }
 
     @Override
