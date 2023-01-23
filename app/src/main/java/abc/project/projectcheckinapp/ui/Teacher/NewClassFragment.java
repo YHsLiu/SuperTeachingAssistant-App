cbd6faf23a58de1a4fafac0a82d0cfcc57408f8c
cindy <cindy0909.liu@gmail.com>
2023-01-23 16:24:42 +0800

    網址還沒改

--- app/src/main/java/abc/project/projectcheckinapp/ui/Teacher/NewClassFragment.java
+++ app/src/main/java/abc/project/projectcheckinapp/ui/Teacher/NewClassFragment.java
@@ -1,14 +1,30 @@
 package abc.project.projectcheckinapp.ui.Teacher;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import abc.project.projectcheckinapp.R;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.concurrent.ExecutorService;
+
+import abc.project.projectcheckinapp.databinding.FragmentNewClassBinding;
+import okhttp3.MediaType;
+import okhttp3.Request;
+import okhttp3.RequestBody;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,51 +32,62 @@ import abc.project.projectcheckinapp.R;
  * create an instance of this fragment.
  */
 public class NewClassFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+    FragmentNewClassBinding binding;
+    NavController navController;
+    SharedPreferences preferences;
+    ExecutorService executor;
     public NewClassFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment NewClassFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static NewClassFragment newInstance(String param1, String param2) {
         NewClassFragment fragment = new NewClassFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_new_class, container, false);
+        binding = FragmentNewClassBinding.inflate(inflater,container,false);
+        preferences = getActivity().getSharedPreferences("userInfo", Context.MODE_PRIVATE);
+        int tid = preferences.getInt("tid",0);
+        String className, classCode;
+        binding.btnTecNCreat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                JSONObject packet = new JSONObject();
+                JSONObject data = new JSONObject();
+                try {
+                    packet.put("type",1);
+                    packet.put("status",10);
+                    data.put("className",binding.txtTecNName.getText().toString());
+                    data.put("classCode",binding.txtTecNCord.getText().toString());
+                    packet.put("classInfo",data);
+                } catch (JSONException e) {
+                    Log.e("error","packet");
+                }
+                MediaType mediaType = MediaType.parse("application/json");
+                RequestBody body = RequestBody.create(packet.toString(),mediaType);
+                Request request= new Request.Builder()
+                        .url("http://192.168.255.62:8864/api/member/login/student")
+                        .post(body)
+                        .build();
+            }
+        });
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        navController = Navigation.findNavController(view);
     }
 }
