a53b295c371b960e02b53f374c9898db41ca6866
cindy <cindy0909.liu@gmail.com>
2023-01-25 17:15:46 +0800

    從Runnable開始尚未改

--- app/src/main/java/abc/project/projectcheckinapp/ui/Teacher/SelectRoomFragment.java
+++ app/src/main/java/abc/project/projectcheckinapp/ui/Teacher/SelectRoomFragment.java
@@ -1,14 +1,38 @@
 package abc.project.projectcheckinapp.ui.Teacher;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.concurrent.ExecutorService;
 
 import abc.project.projectcheckinapp.R;
+import abc.project.projectcheckinapp.databinding.FragmentSelectRoomBinding;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,50 +41,107 @@ import abc.project.projectcheckinapp.R;
  */
 public class SelectRoomFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+    FragmentSelectRoomBinding binding;
+    NavController navController;
+    SharedPreferences preferences;
+    SharedPreferences.Editor contextEditor;
+    ExecutorService executor;
+    Handler selectClassResultHandler = new Handler(Looper.getMainLooper()){
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+            Bundle bundle = msg.getData();
+            if (bundle.getInt("status" )==11) // 新增課程成功
+            {   builder = new AlertDialog.Builder(getActivity());
+                builder.setTitle("新增成功");
+                builder.setMessage("直接進入教室嗎?");
+                builder.setPositiveButton("進教室", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        preferences = getActivity().getSharedPreferences("userInfo",Context.MODE_PRIVATE);
+                        contextEditor = preferences.edit();
+                        contextEditor.putInt("cid",bundle.getInt("cid")).apply();
+                        navController.navigate(R.id.action_nav_tec_newclass_to_nav_tec_enter);
+                    }
+                });
+                builder.setNegativeButton("關閉", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {    }
+                });
+                dialog = builder.create();
+                dialog.show();
+            } else {
+                Toast.makeText(getActivity(), "新增失敗,代碼已被使用", Toast.LENGTH_LONG).show();
+            }
+        }
+    };
     public SelectRoomFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SelectRoomFragment.
-     */
-    // TODO: Rename and change types and number of parameters
+
     public static SelectRoomFragment newInstance(String param1, String param2) {
         SelectRoomFragment fragment = new SelectRoomFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_select_room, container, false);
+        binding = FragmentSelectRoomBinding.inflate(inflater,container,false);
+        preferences = getActivity().getSharedPreferences("userInfo", Context.MODE_PRIVATE);
+        int tid = preferences.getInt("tid",0);
+        JSONObject packet = new JSONObject();
+        try {
+            packet.put("type",1);
+            packet.put("status",11);
+            packet.put("tid",tid);
+        } catch (JSONException e) {
+            Log.e("error","packet");
+        }
+        MediaType mediaType = MediaType.parse("application/json");
+        RequestBody body = RequestBody.create(packet.toString(),mediaType);
+        Request request= new Request.Builder()
+                .url("http://192.168.255.62:8864/api/list/classroom")
+                .post(body)
+                .build();
+        SimpleAPIWorker apiCaller = new SimpleAPIWorker(request);
+        executor.execute(apiCaller);
+        return binding.getRoot();
+    }
+
+    class SimpleAPIWorker implements Runnable{
+        OkHttpClient client;
+        Request request;
+        public SimpleAPIWorker(Request request) {
+            this.request = request;
+            client = new OkHttpClient();
+        }
+        @Override
+        public void run() {
+            try {
+                Response response = client.newCall(request).execute();
+                JSONObject result = new JSONObject(response.body().string());
+                Message m = selectClassResultHandler.obtainMessage();
+                Bundle bundle = new Bundle();
+                bundle.putInt("status",result.getInt("status") );
+                bundle.putInt("cid",result.getInt("cid") );
+                m.setData(bundle);
+                selectClassResultHandler.sendMessage(m);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        navController = Navigation.findNavController(view);
     }
 }
