bb23b12352145414c192c92a0d5eda53de06efa3
Cindy <cindy0909.liu@gmail.com>
2023-01-27 17:21:58 +0800

    做到Handler

--- app/src/main/java/abc/project/projectcheckinapp/ui/Teacher/RecordFragment.java
+++ app/src/main/java/abc/project/projectcheckinapp/ui/Teacher/RecordFragment.java
@@ -1,14 +1,41 @@
 package abc.project.projectcheckinapp.ui.Teacher;
 
+import static android.content.Context.MODE_PRIVATE;
+
+import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import abc.project.projectcheckinapp.R;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+
+import abc.project.projectcheckinapp.databinding.FragmentRecordBinding;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,50 +44,122 @@ import abc.project.projectcheckinapp.R;
  */
 public class RecordFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    FragmentRecordBinding binding;
+    NavController navController;
+    SharedPreferences preferences;
+    ExecutorService executor;
+    RecyclerView recyclerView;
+    SQLiteDatabase db;
+    int cid;
+    Handler OpenResultHandler = new Handler(Looper.getMainLooper()){
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+            Bundle bundle = msg.getData();
+            preferences = getActivity().getSharedPreferences("userInfo",MODE_PRIVATE);
+            int cid = preferences.getInt("cid",0);
+            db = getActivity().openOrCreateDatabase("allList",MODE_PRIVATE,null); // 將學生資訊存進裝置的DB
+            // 每個跟RecyclerView有關的資料都存進裝置名為 allList 的DB中，以table名稱來區別每個list
+            db.execSQL("drop table if exists "+cid+"_record_semester;");
+            db.execSQL("create table "+cid+"_no_rcstu(sid integer,name text, depart text, stuId text);");
+            try {
+                JSONArray Info = new JSONArray(bundle.getString("list"));
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+            } catch (JSONException e) {
+                throw new RuntimeException(e);
+            }
 
+        }
+    };
     public RecordFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment RecordFragment.
-     */
-    // TODO: Rename and change types and number of parameters
+
     public static RecordFragment newInstance(String param1, String param2) {
         RecordFragment fragment = new RecordFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_record, container, false);
+        binding = FragmentRecordBinding.inflate(inflater, container, false);
+        preferences = getActivity().getSharedPreferences("userInfo",MODE_PRIVATE);
+        cid = preferences.getInt("cid",0);
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMdd");
+        String date = formatter.format(new Date());
+        MediaType mediaType = MediaType.parse("application/json");
+        JSONObject packet = new JSONObject();
+        JSONObject data = new JSONObject();
+        try {
+            packet.put("type", 1);
+            packet.put("status", 11);
+            data.put("cid", cid);
+            data.put("date", date);
+            packet.put("data", data);
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+        RequestBody body = RequestBody.create(packet.toString(), mediaType);
+        binding.btnSemeseterRecord.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Request request = new Request.Builder()
+                        .url("http://192.168.255.62:8864/api/record/semester")
+                        .post(body)
+                        .build();
+                SimpleAPIWorker apiCaller = new SimpleAPIWorker(request);
+                executor.execute(apiCaller);
+            }
+        });
+
+        binding.btnTodayRecord.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Request request = new Request.Builder()
+                        .url("http://192.168.255.62:8864/api/record/today")
+                        .post(body)
+                        .build();
+                SimpleAPIWorker apiCaller = new SimpleAPIWorker(request);
+                executor.execute(apiCaller);
+            }
+        });
+        return binding.getRoot();
+    }
+    class SimpleAPIWorker implements Runnable {
+        OkHttpClient client;
+        Request request;
+        public SimpleAPIWorker(Request request) {
+            this.request = request;
+            client = new OkHttpClient();
+        }
+        @Override
+        public void run() {
+            try {
+                Response response = client.newCall(request).execute();
+                JSONObject result = new JSONObject(response.body().string());
+                Message m = RecordHandler.obtainMessage();
+                Bundle bundle = new Bundle();
+                bundle.putInt("list", result.getInt("list"));
+                m.setData(bundle);
+                RecordHandler.sendMessage(m);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        navController = Navigation.findNavController(view);
     }
 }
