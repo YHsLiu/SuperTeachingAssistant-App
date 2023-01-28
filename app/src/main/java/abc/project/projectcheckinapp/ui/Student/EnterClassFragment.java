4ad0cc0d3e3e2c8cc5ef6aa2d6a0dca9ba9bbfe7
cindy <cindy0909.liu@gmail.com>
2023-01-28 14:35:17 +0800

    學生進入教室完成(122行需加actionID)

--- app/src/main/java/abc/project/projectcheckinapp/ui/Student/EnterClassFragment.java
+++ app/src/main/java/abc/project/projectcheckinapp/ui/Student/EnterClassFragment.java
@@ -1,6 +1,15 @@
 package abc.project.projectcheckinapp.ui.Student;
 
+import static android.content.Context.MODE_PRIVATE;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,18 +19,66 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.concurrent.ExecutorService;
 
 import abc.project.projectcheckinapp.R;
 import abc.project.projectcheckinapp.databinding.FragmentEnterclassBinding;
+import abc.project.projectcheckinapp.rawData.AdapterClassroom;
+import abc.project.projectcheckinapp.rawData.ClickListener;
+import abc.project.projectcheckinapp.ui.Teacher.SelectRoomFragment;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class EnterClassFragment extends Fragment {
 
     private FragmentEnterclassBinding binding;
     NavController navController;
+    int sid;
+    SQLiteDatabase db;
+    SharedPreferences preferences;
+    RecyclerView recyclerView;
+    ClickListener clickListener;
+    SharedPreferences.Editor contextEditor;
+    ExecutorService executor;
 
     public EnterClassFragment() {
     }
 
+    Handler enterClassResultHandler = new Handler(Looper.getMainLooper()){
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+            Bundle bundle = msg.getData();
+            preferences = getActivity().getSharedPreferences("userInfo",MODE_PRIVATE);
+            sid = preferences.getInt("sid",0);
+            db = getActivity().openOrCreateDatabase("allList",MODE_PRIVATE,null); // 將學生資訊存進裝置的DB
+            // 每個跟RecyclerView有關的資料都存進裝置名為 allList 的DB中，以table名稱來區別每個list
+            db.execSQL("drop table if exists allroom;");
+            db.execSQL("create table allroom(cid integer,classname text, classcode text);");
+            try {
+                JSONArray roomInfos =new JSONArray( bundle.getString("roomList") );
+                for (int i = 0; i<roomInfos.length();i++){
+                    JSONObject stuInfo = roomInfos.getJSONObject(i);
+                    db.execSQL("insert into allroom values (?,?,?);",
+                            new Object[] { stuInfo.getInt("cid"),
+                                    stuInfo.getString("課程名稱"),
+                                    stuInfo.getString("課程代碼")}); }
+                db.close();
+            } catch (JSONException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    };
     public static EnterClassFragment newInstance(String param1, String param2) {
         EnterClassFragment fragment = new EnterClassFragment();
         Bundle args = new Bundle();
@@ -34,17 +91,77 @@ public class EnterClassFragment extends Fragment {
 
         binding = FragmentEnterclassBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        preferences = getActivity().getSharedPreferences("userInfo", Context.MODE_PRIVATE);
+        sid = preferences.getInt("sid",0);
+        JSONObject packet = new JSONObject();
+        try {
+            packet.put("type",1);
+            packet.put("status",11);
+            packet.put("sid",sid);
+        } catch (JSONException e) {
+            Log.e("error","packet");
+        }
+        MediaType mediaType = MediaType.parse("application/json");
+        RequestBody body = RequestBody.create(packet.toString(),mediaType);
+        Request request= new Request.Builder()
+                .url("http://192.168.255.62:8864/api/list/student/classroom")
+                .post(body)
+                .build();
+        EnterClassAPIWorker apiCaller = new EnterClassAPIWorker(request);
+        executor.execute(apiCaller);
 
-        binding.button2.setOnClickListener(new View.OnClickListener() {
+        db = getActivity().openOrCreateDatabase("allList",MODE_PRIVATE,null);
+        recyclerView =binding.RecyclerStuClassroom;
+        clickListener = new ClickListener() {
+            @Override
+            public void onClickForAllStuList(int position, int sid, String stuname, String studepart, String stuid) {   }
+            @Override
+            public void onClickForClassroom(int position, int cid) {
+                contextEditor = preferences.edit();
+                contextEditor.putInt("cid",cid);
+                navController.navigate(R.id.);  // 你看要連哪個action
+            }
+            @Override
+            public void onClickForNoRcStuList(int position, int sid) {   }
+        };
+        AdapterClassroom adapter = new AdapterClassroom(db,clickListener);
+        binding.btnStuSQuery.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // 使用 NavController 切換到 classTableFragement
-                // 請使用 Mobile_navigation 中的 Action ID
-                navController.navigate(R.id.nav_stdRollCall);
+                adapter.selectRoom(binding.txtStuSSelect.getText().toString());
+                adapter.notifyDataSetChanged();
             }
         });
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
         return root;
+    }
+
+    class EnterClassAPIWorker implements Runnable {
+        OkHttpClient client;
+        Request request;
+
+        public EnterClassAPIWorker(Request request) {
+            this.request = request;
+            client = new OkHttpClient();
+        }
+
+        @Override
+        public void run() {
+            try {
+                Response response = client.newCall(request).execute();
+                JSONObject result = new JSONObject(response.body().string());
+                Message m = enterClassResultHandler.obtainMessage();
+                Bundle bundle = new Bundle();
+                bundle.putInt("status", result.getInt("type"));
+                bundle.putInt("roomList", result.getInt("list"));
+                m.setData(bundle);
+                enterClassResultHandler.sendMessage(m);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 
     @Override
