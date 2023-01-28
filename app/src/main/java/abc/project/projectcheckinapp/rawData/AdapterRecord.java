73349f0a69f0b4aa5c864cae6d0d466935ed6941
cindy <cindy0909.liu@gmail.com>
2023-01-28 19:04:00 +0800

    學生點名紀錄 1/28 19:03

--- app/src/main/java/abc/project/projectcheckinapp/rawData/AdapterRecord.java
+++ app/src/main/java/abc/project/projectcheckinapp/rawData/AdapterRecord.java
@@ -19,19 +19,19 @@ public class AdapterRecord extends RecyclerView.Adapter<AdapterRecord.ViewHolder
     SQLiteDatabase db;
     String dateR;
     int cid;
+    int sid;
     ArrayList<RecordModel> result;
 
     public AdapterRecord(SQLiteDatabase db, int cid) {  // 學期紀錄
         this.db = db;
         this.cid = cid;
         result = new ArrayList<>();
-        Cursor cursor = db.rawQuery("select distinct date from "+cid+"_record_semester;",null);
-        Cursor cursor1;
+        Cursor cursor = db.rawQuery("select distinct rc_date from "+cid+"_record_semester;",null);
         if (cursor.getCount() > 0){
             cursor.moveToFirst();
             do {
                 dateR = cursor.getString(0);
-                cursor1 = db.rawQuery("select stuId,name from "+cid+"_record_semester where date="+dateR+" order by stuId;", null);
+                Cursor cursor1 = db.rawQuery("select stuId,name from "+cid+"_record_semester where rc_date="+dateR+" order by stuId;", null);
                 String stuInfo = "";
                 cursor1.moveToFirst();
                 do {
@@ -39,10 +39,10 @@ public class AdapterRecord extends RecyclerView.Adapter<AdapterRecord.ViewHolder
                 } while ((cursor1.moveToNext()));
                 RecordModel recordModel = new RecordModel(dateR, stuInfo);
                 result.add(recordModel);
+                cursor1.close();
             }while (cursor.moveToNext());
-            cursor1.close();
+            cursor.close();
         }
-        cursor.close();
     }
 
     public AdapterRecord(SQLiteDatabase db, String dateR, int cid) {  // 今日紀錄
@@ -59,8 +59,33 @@ public class AdapterRecord extends RecyclerView.Adapter<AdapterRecord.ViewHolder
             }while (cursor.moveToNext());
             RecordModel recordModel = new RecordModel(dateR, stuInfo);
             result.add(recordModel);
+            cursor.close();
         }
-        cursor.close();
+    }
+
+    public AdapterRecord(SQLiteDatabase db, int cid, int sid) {
+        this.db = db;
+        this.cid = cid;
+        this.sid = sid;
+        result = new ArrayList<>();
+        Cursor cursor = db.rawQuery("select rc_date from "+cid+"_record_student where sid=0;", null);
+        if (cursor.getCount() > 0){
+            cursor.moveToFirst();
+            do {
+                dateR = cursor.getString(0);
+                Cursor cursor1 = db.rawQuery("select count(*) from "+cid+"_record_student where sid="+sid+" and rc_date="+dateR+";", null);
+                String isAttend = "";
+                if (cursor1.getCount()==1){
+                    isAttend = "出席";
+                } else if (cursor1.getCount()==0) {
+                    isAttend = "缺席";
+                }
+                RecordModel recordModel = new RecordModel(dateR, isAttend);
+                result.add(recordModel);
+                cursor1.close();
+            } while (cursor.moveToNext());
+            cursor.close();
+        }
     }
 
     @NonNull
--- app/src/main/java/abc/project/projectcheckinapp/ui/Student/StdRecordFragment.java
+++ app/src/main/java/abc/project/projectcheckinapp/ui/Student/StdRecordFragment.java
@@ -3,6 +3,7 @@ package abc.project.projectcheckinapp.ui.Student;
 import static android.content.Context.MODE_PRIVATE;
 
 import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -10,17 +11,29 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.concurrent.ExecutorService;
 
 import abc.project.projectcheckinapp.databinding.FragmentStdRecordBinding;
+import abc.project.projectcheckinapp.rawData.AdapterRecord;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class StdRecordFragment extends Fragment {
 
@@ -29,12 +42,36 @@ public class StdRecordFragment extends Fragment {
     SharedPreferences preferences;
     ExecutorService executor;
     RecyclerView recyclerView;
+    AdapterRecord adapter;
+    SQLiteDatabase db;
     int cid, sid;
 
     public StdRecordFragment() {
         // Required empty public constructor
     }
 
+    Handler RecordHandler = new Handler(Looper.getMainLooper()){
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+            Bundle bundle = msg.getData();
+            preferences = getActivity().getSharedPreferences("userInfo",MODE_PRIVATE);
+            int cid = preferences.getInt("cid",0);
+            JSONArray stuRrcord;
+            db = getActivity().openOrCreateDatabase("allList",MODE_PRIVATE,null);
+            // semester紀錄
+            db.execSQL("drop table if exists " + cid + "_record_student;");
+            db.execSQL("create table " + cid + "_record_student(sid int,rc_date text);");
+            try { stuRrcord = new JSONArray(bundle.getString("list"));
+                for (int i =0; i<stuRrcord.length(); i++) {
+                    JSONObject recordInfo = stuRrcord.getJSONObject(i);
+                    db.execSQL("insert into "+cid+"_record_student values (?,?);",
+                            new Object[] { recordInfo.getInt("sid"),
+                                    recordInfo.getString("日期")});  }
+            } catch (JSONException e) {  throw new RuntimeException(e);   }
+            db.close();
+        }
+    };
     public static StdRecordFragment newInstance(String param1, String param2) {
         StdRecordFragment fragment = new StdRecordFragment();
         Bundle args = new Bundle();
@@ -45,13 +82,8 @@ public class StdRecordFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
         //時間存至共用sharedPreferences
         //preferences.edit().putString("data",currentDate).apply();
-
-
-
     }
 
     @Override
@@ -61,23 +93,60 @@ public class StdRecordFragment extends Fragment {
         preferences = getActivity().getSharedPreferences("userInfo",MODE_PRIVATE);
         cid = preferences.getInt("cid",0);
         sid = preferences.getInt("sid",0);
-
+        recyclerView = binding.recyclerView;
 
         binding.btnStdRecord.setOnClickListener(new View.OnClickListener() {
-            @Override        //要改
+            @Override
             public void onClick(View v) {
+                MediaType mediaType = MediaType.parse("application/json");
+                JSONObject packet = new JSONObject();
+                JSONObject data = new JSONObject();
+                try {
+                    packet.put("type", 1);
+                    packet.put("status", 11);
+                    data.put("cid", cid);
+                    data.put("sid", sid);
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
                 RequestBody body = RequestBody.create(packet.toString(), mediaType);
                 Request request = new Request.Builder()
-                        .url("http://192.168.255.67:8864/api/project/stdRecord")
+                        .url("http://192.168.255.67:8864/api/record/student")
                         .post(body)
                         .build();
-                /*SimpleAPIWorker apiCaller = new SimpleAPIWorker(request);
-                executor.execute(apiCaller);*/
+                SimpleAPIWorker apiCaller = new SimpleAPIWorker(request);
+                executor.execute(apiCaller);
+                db = getActivity().openOrCreateDatabase("allList",MODE_PRIVATE,null);
+                adapter = new AdapterRecord(db,cid,sid);
+                recyclerView.setAdapter(adapter);
+                recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+                db.close();
             }
         });
-
-
         return binding.getRoot();
+    }
+    class SimpleAPIWorker implements Runnable {
+        OkHttpClient client;
+        Request request;
+        public SimpleAPIWorker(Request request) {
+            this.request = request;
+            client = new OkHttpClient();
+        }
+        @Override
+        public void run() {
+            try {
+                Response response = client.newCall(request).execute();
+                JSONObject result = new JSONObject(response.body().string());
+                Message m = RecordHandler.obtainMessage();
+                Bundle bundle = new Bundle();
+                bundle.putInt("type", result.getInt("type"));
+                bundle.putString("list", result.getString("list"));
+                m.setData(bundle);
+                RecordHandler.sendMessage(m);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 
     @Override
