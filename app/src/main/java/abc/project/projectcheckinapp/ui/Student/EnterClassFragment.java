c49abadac0e210e540e0907070392f87bf7a60e1
Cindy <cindy0909.liu@gmail.com>
2023-01-30 17:29:56 +0800

    1/30 17:05 學生進教室成功

--- app/src/main/java/abc/project/projectcheckinapp/ui/Student/EnterClassFragment.java
+++ app/src/main/java/abc/project/projectcheckinapp/ui/Student/EnterClassFragment.java
@@ -74,7 +74,16 @@ public class EnterClassFragment extends Fragment {
                             new Object[] { stuInfo.getInt("cid"),
                                     stuInfo.getString("課程名稱"),
                                     stuInfo.getString("課程代碼")}); }
-                db.close();
+                AdapterClassroom adapter = new AdapterClassroom(db,clickListener);
+                binding.btnStuSQuery.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        adapter.selectRoom(binding.txtStuSSelect.getText().toString());
+                        adapter.notifyDataSetChanged();
+                    }
+                });
+                recyclerView.setAdapter(adapter);
+                recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
             } catch (JSONException e) {
                 throw new RuntimeException(e);
             }
@@ -95,6 +104,7 @@ public class EnterClassFragment extends Fragment {
         preferences = getActivity().getSharedPreferences("userInfo", Context.MODE_PRIVATE);
         executor = Executors.newSingleThreadExecutor();
         sid = preferences.getInt("sid",0);
+        recyclerView =binding.RecyclerStuClassroom;
         JSONObject packet = new JSONObject();
         try {
             packet.put("type",1);
@@ -106,14 +116,12 @@ public class EnterClassFragment extends Fragment {
         MediaType mediaType = MediaType.parse("application/json");
         RequestBody body = RequestBody.create(packet.toString(),mediaType);
         Request request= new Request.Builder()
-                .url("http://192.168.255.67:8864/api/list/student/classroom")
+                .url("http://192.168.255.62:8864/api/list/student/classroom")
                 .post(body)
                 .build();
         EnterClassAPIWorker apiCaller = new EnterClassAPIWorker(request);
         executor.execute(apiCaller);
 
-        db = getActivity().openOrCreateDatabase("allList",MODE_PRIVATE,null);
-        recyclerView =binding.RecyclerStuClassroom;
         clickListener = new ClickListener() {
             @Override
             public void onClickForAllStuList(int position, int sid, String stuname, String studepart, String stuid) {   }
@@ -128,17 +136,6 @@ public class EnterClassFragment extends Fragment {
             @Override
             public void onClickForNoRcStuList(int position, int sid) {   }
         };
-        AdapterClassroom adapter = new AdapterClassroom(db,clickListener);
-        binding.btnStuSQuery.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                adapter.selectRoom(binding.txtStuSSelect.getText().toString());
-                adapter.notifyDataSetChanged();
-            }
-        });
-        recyclerView.setAdapter(adapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-
         return root;
     }
 
@@ -155,11 +152,13 @@ public class EnterClassFragment extends Fragment {
         public void run() {
             try {
                 Response response = client.newCall(request).execute();
-                JSONObject result = new JSONObject(response.body().string());
+                String data = response.body().string();
+                Log.w("api","學生進教室 api回傳:"+data);
+                JSONObject result = new JSONObject(data);
                 Message m = enterClassResultHandler.obtainMessage();
                 Bundle bundle = new Bundle();
                 bundle.putInt("status", result.getInt("type"));
-                bundle.putInt("roomList", result.getInt("list"));
+                bundle.putString("roomList", result.getString("list"));
                 m.setData(bundle);
                 enterClassResultHandler.sendMessage(m);
             } catch (Exception e) {
